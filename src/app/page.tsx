dcfac4e696995aae3d473ec4dfd47052d97b2568
Kasunisarangi <kasuni.aventure@gmail.com>
2025-03-06 13:29:11 +0530

    set up layout

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Hero from "./sections/Hero";
+
+export default function Home() {
+  return (
+    <div className="overflow-x-hidden">
+      <Hero />
+    </div>
+  );
+}
---
+++ src/app/sections/Hero.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+const Hero = () => {
+  return <div className="bg-amber-700">Hero</div>;
+};
+
+export default Hero;

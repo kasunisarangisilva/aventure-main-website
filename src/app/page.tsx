129c13b034d607ec720375affd288f2d2525ec90
Kasunisarangi <kasuni.aventure@gmail.com>
2025-03-07 10:54:36 +0530

    update code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Hero from "./sections/Hero";
-// import Section2 from "./sections/Section2";
 
 export default function Home() {
   return (
     <div className="overflow-x-hidden">
       <Hero />
-      {/* <Section2 id="section2" /> */}
     </div>
   );
 }
